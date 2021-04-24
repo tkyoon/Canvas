45aa81aa98975a3c795871797d5e7c46e38e7775
Tk yoon <y-friend@daum.net>
2021-04-24 21:29:25 +0900

    fix : mkdir async로 변경

--- util/logger.js
+++ util/logger.js
@@ -1,43 +1,40 @@
 /** logger  [TK Yoon 2018. 10. 13. 오전 9:17:41] */
- 
 
-var fs 		= require('fs');
-var moment 	= require('moment');
+var fs = require("fs");
+var moment = require("moment");
 
 //log 폴더 없다면 생성
 try {
-	
-	if (!fs.existsSync('./log')) {
-		fs.mkdir('./log');
-	}
-} catch(err) {
-  console.error(err)
-  
+  if (!fs.existsSync("./log")) {
+    fs.mkdirSync("./log");
+  }
+} catch (err) {
+  console.error(err);
 }
 
-var log = require('tracer').colorConsole({
-	transport: [
-		function (data) {
-			fs.appendFile('./log/pilsa.'+ moment(data.timestame).format('YYYY-MM-DD') +'.log', data.rawoutput + '\n', (err) => {
-				//if (err) throw err;
-				if (err) console.error(err);
-			});
-		}
-		, function(data) {
-			console.log(data.output);
-		}
-	]
-	, format : [
-		//"{{timestamp}} <{{title}}>:{{message}} (in {{file}}:{{line}})" //default format
-		"[{{timestamp}}] [{{title}} {{path}}:{{line}}] - {{message}}"
-		, {
-			error : "[{{timestamp}}] [{{title}} {{path}}:{{line}}] - {{message}}\nCall Stack:\n{{stack}}" // error format
-		}
-	]
-	, dateformat : "yy-mm-dd HH:MM:ss.l"
-	, preprocess :  function(data){
-		data.title = data.title.toUpperCase();
-	}
+var log = require("tracer").colorConsole({
+  transport: [
+    function (data) {
+      fs.appendFile("./log/cavas." + moment(data.timestame).format("YYYY-MM-DD") + ".log", data.rawoutput + "\n", (err) => {
+        //if (err) throw err;
+        if (err) console.error(err);
+      });
+    },
+    function (data) {
+      console.log(data.output);
+    },
+  ],
+  format: [
+    //"{{timestamp}} <{{title}}>:{{message}} (in {{file}}:{{line}})" //default format
+    "[{{timestamp}}] [{{title}} {{path}}:{{line}}] - {{message}}",
+    {
+      error: "[{{timestamp}}] [{{title}} {{path}}:{{line}}] - {{message}}\nCall Stack:\n{{stack}}", // error format
+    },
+  ],
+  dateformat: "yy-mm-dd HH:MM:ss.l",
+  preprocess: function (data) {
+    data.title = data.title.toUpperCase();
+  },
 });
 
 module.exports = log;
